e997e5fa4663cbba1291d5ef7d6a96910bba64f5
alessiotinaglia <alessio.tina1998@gmail.com>
2024-12-02 16:58:39 +0100

    fine terzo punto

--- server.js
+++ server.js
@@ -3,10 +3,56 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
+// Array di post
+const posts = [
+    {
+        titolo: "Il mio primo post",
+        contenuto: "Questo è il contenuto del mio primo post. Parlerò di come iniziare con Node.js.",
+        immagine: "",
+        tags: ["Node.js", "Introduzione", "Backend"]
+    },
+    {
+        titolo: "Secondo post",
+        contenuto: "Questo è il contenuto del mio primo post. Parlerò di come iniziare con Node.js.",
+        immagine: "",
+        tags: ["Node.js", "Introduzione", "Backend"]
+    },
+    {
+        titolo: "Terzo post",
+        contenuto: "Questo è il contenuto del mio primo post. Parlerò di come iniziare con Node.js.",
+        immagine: "",
+        tags: ["Node.js", "Introduzione", "Backend"]
+    },
+    {
+        titolo: "Quarto post",
+        contenuto: "Questo è il contenuto del mio primo post. Parlerò di come iniziare con Node.js.",
+        immagine: "",
+        tags: ["Node.js", "Introduzione", "Backend"]
+    },
+    {
+        titolo: "Quinto post",
+        contenuto: "Questo è il contenuto del mio primo post. Parlerò di come iniziare con Node.js.",
+        immagine: "",
+        tags: ["Node.js", "Introduzione", "Backend"]
+    }
+];
+
+// Rotta principale
 app.get("/", (req, res) => {
     res.send("<h1>Server del mio blog</h1>");
-  });
+});
 
+// Rotta per ottenere tutti i post
+app.get("/posts", (req, res) => {
+    res.json(posts); // Restituisce i post come oggetti JSON
+});
+
+// Fallback per rotte non trovate
+app.all("*", (req, res) => {
+    res.status(404).send("<h1>Error 404 - Not Found!</h1>");
+});
+
+// In ascolto sulle rotte
 app.listen(PORT, () => {
-    console.log(`Server del mio e : http://localhost:${PORT}}`);
+    console.log(`Server del mio blog in esecuzione su http://localhost:${PORT}`);
 });
